6201451bf17f136123dacd4e36a966d97809f3f3
МухинаЛВ@ngknn.local <@ngknn.local>
2022-01-26 08:05:21 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp2
+{
+   static class Model
+    {
+        // данные
+      public  static int count = 0;
+      public  static List<string> dataList = new List<string> { "Иванов", "Петров", "Сидоров" };       
+    }
+}
---
+++ WpfApp2/ViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string CountChange
+        {
+            get
+            {
+                return Model.count.ToString();
+            }
+        }
+        public List<string> ComboChange
+        {
+            get
+            {
+                return Model.dataList;
+            }
+        }
+        int cbIndex = -1;
+        public int IndexSelected
+        {
+            set
+            {
+                // индек - это необходимое значение, которое нужно получить
+                cbIndex = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("CBIndex"));
+            }
+        }
+        public string CBIndex
+        {
+            get
+            {
+                if (cbIndex == -1)
+                {
+                    return "";
+                }
+                else
+                {
+                    return Model.dataList[cbIndex];
+                }
+                
+            }
+        }
+        // класс, который реализазует интерфейс ICommand
+        public RoutedCommand Command { get; set; } = new RoutedCommand();
+        public void Command_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Model.count++;
+            PropertyChanged(this, new PropertyChangedEventArgs("CountChange"));
+        }
+        public CommandBinding bind; // создание объекта для привязки команды
+        public ViewModel()
+        {
+            bind = new CommandBinding(Command);
+            bind.Executed += Command_Executed;
+        }
+    }     
+}
